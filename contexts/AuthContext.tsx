b59e3a152bd94649a4b67cdcc8dbe7b8d0152c28
Thiago <thiagofreitascarneiro@hotmail.com>
2022-04-05 21:34:58 -0300

    JWT auth

---
+++ contexts/AuthContext.tsx
@@ -0,0 +1,31 @@
+import { createContext, ReactNode } from 'react';
+
+type SignCredentials = {
+    email: string;
+    password: string;
+}
+
+type AuthContextData = {
+    signIn(credentials: SignCredentials): Promise<void>;
+    isAuthenticated: boolean;
+}
+
+type AuthProviderPRops = {
+    children: ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextData)
+
+export function AuthProvider({ children }: AuthProviderPRops) {
+    const isAuthenticated = false;
+
+    async function signIn({ email, password }: SignCredentials) {
+        console.log({ email, password})
+    }
+
+    return (
+        <AuthContext.Provider value={{ signIn, isAuthenticated}}>
+            { children } 
+        </AuthContext.Provider>
+    )
+}
---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import { AppProps } from 'next/app'
+import { AuthProvider } from '../contexts/AuthContext'
+
+
+function MyApp({ Component, pageProps }: AppProps ) {
+  return (
+    <>
+    <AuthProvider>
+     <  Component {...pageProps} />
+     </AuthProvider>
+   </>
+  
+  )
+}
+
+export default MyApp
